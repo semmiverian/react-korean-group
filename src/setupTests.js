67c361d5c5ec3cc6286b316b59d255a8aed8bf39
Semmiverian <semmivp1@gmail.com>
2019-03-10 21:44:03 +0700

    create starting pack

--- src/setupTests.js
+++
@@ -1,7 +0,0 @@
-// mengimport cleanup - after - each
-// jadi DOM kita akan selaulu dibersihkan setiap kali kita
-// selesai membuat sebuah testimport 'react-testing-library/cleanup-after-each'
-import 'react-testing-library/cleanup-after-each'
-
-// Menambahkan assertion khusus yang dibuat oleh jest-dom
-import 'jest-dom/extend-expect'
